bebaa80b697dc7439d8d1f0ff5be75a7ef72596a
backrooms9000 <Geancaio926@gmail.com>
2026-08-10 21:28:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,77 @@
-# Primeiro teste com variávei
-"nome": = [
-{"blusa",
-"codigo": "3uhh8h3rp98h8h8h8ri",
-"preco": 1000.00,
-"quantidade": 500,
-"categoria": "roupas"},
-{}
-]
- this
+import subprocess
+import random
+
+#numero = random.randint(1, 1000)
+
+#baco de dados
+database = {
+    "1": { "name": "Rex", "phone": "(21)3456789", "}`,
+    "120": { "name": "maria simpa", "contact": "(21)9876203" },
+def cls():
+    subprocess.run("cls", shell=True)
+
+def new_contact():
+    cls()
+    print("[ AGENDA FURRECA - NOVO CONTATO ]")
+
+    print("\nDiguite os dados do contato\n")
+
+    name + input(" ")
+
+    #...
+
+def list_contacts():
+    cls()
+    print("[ AGENDA FURRECA - LISTAR CONTATOS ]")
+
+    #...
+
+def edit_contact():
+    cls()
+    print("[ AGENDA FURRECA - EDITAR CONTATO ]")
+
+    #...
+
+def delete_contact():
+    cls()
+    print("[ AGENDA FURRECA - APAGAR CONTATO ]")
+
+    #...
+
+  # programa principal
+  def main(erro = str()):
+    #main loop
+    while true:
+    cls()
+    print("[ AGENDA FURRECA - MENU PRINCIPAL ]")
+    print(erro)
+    erro = str()
+    print('''
+Opcoes:
+
+1-novo contato
+2-listar contatos
+3-Editar contato
+4- Apagar contato
+0-Sir do programa
+    ''')
+
+        opcao = imput("Escolha uma opcao: ")
+
+        match opcao:
+        case 1:
+           new_contact()
+        case 2:
+           list_contacts()
+        case 3:
+           edit_contact()
+        case 0:
+            cls()
+            print("\Acabou!")
+            exist()
+        case_:
+            erro = "digite uma opcao valida!"
+            main(erro)
+
+# "Roda" o programa
+main()
